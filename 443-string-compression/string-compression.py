87fd6499652ae187e28f97a2fdfaa5234fa2b8dc
Sabari Kumar <sabari1082005@gmail.com>
2026-03-28 01:29:27 +0530

    Time: 2 ms (67.57%) | Memory: 12.4 MB (93.56%) - LeetSync

---
+++ 443-string-compression/string-compression.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        i = 0   
+        j = 0   
+        while j < len(chars):
+            char = chars[j]
+            count = 0
+            while j < len(chars) and chars[j] == char:
+                j += 1
+                count += 1
+            chars[i] = char
+            i += 1
+            if count > 1:
+                for c in str(count):
+                    chars[i] = c
+                    i += 1
+        return i
+        
